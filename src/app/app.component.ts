6d87e898cf9c4b745701bf8b26e7b7668b0b2735
RominaNavarrete <rominagnavarrete@gmail.com>
2024-04-27 07:08:51 -0300

    components and routes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { NavComponent } from './components/shared/nav/nav.component';
+import { FooterComponent } from './components/shared/footer/footer.component';
+import { HomeComponent } from './components/pages/home/home/home.component';
+
+
+
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [CommonModule, RouterOutlet , NavComponent, HomeComponent, FooterComponent],
+
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { NavComponent } from './components/shared/navbar/nav/nav.component';
+import { FooterComponent } from './components/shared/footer/footer.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:'nav', component:NavComponent},
+    // {path:'footer', component:FooterComponent},
+
+];
